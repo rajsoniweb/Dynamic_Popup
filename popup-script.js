f7fbe735619c15bf07243d7bcd79c0bf9f16b459
Raj Soni <142746668+rajsoniweb@users.noreply.github.com>
2024-10-17 14:20:35 +0530

    Update popup-script.js

--- popup-script.js
+++ popup-script.js
@@ -13,29 +13,41 @@ class PopupHandler {
         });
     }
 
-    svgIcons = {
-        confirm: `
-        <svg width="90" height="68" style="margin: -18px 0px -5px -25px;" xmlns="http://www.w3.org/2000/svg">
-                <rect width="40" height="40" x="25" y="25" fill="#ffc107" transform="rotate(45 60 60)"></rect>
-                <text x="60" y="42" font-size="27" font-weight="bold" text-anchor="middle" dominant-baseline="middle" fill="white">?</text>
-            </svg>`,
-        success: `
-            <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 24 24">
-                <circle cx="12" cy="12" r="10" fill="#07a545" stroke="#07a545" stroke-width="2"/>
-                <path d="M8 12.5l3 3 5-6" fill="none" stroke="#fff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-            </svg>`,
-        failed: `
-            <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="#d40404" viewBox="0 0 24 24">
-                <path fill-rule="evenodd" d="M2 12C2 6.477 6.477 2 12 2s10 4.477 10 10-4.477 10-10 10S2 17.523 2 12Zm7.707-3.707a1 1 0 0 0-1.414 1.414L10.586 12l-2.293 2.293a1 1 0 1 0 1.414 1.414L12 13.414l2.293 2.293a1 1 0 0 0 1.414-1.414L13.414 12l2.293-2.293a1 1 0 0 0-1.414-1.414L12 10.586 9.707 8.293Z" clip-rule="evenodd"/>
-            </svg>`,
-        warning: `
-            <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="#ff9800" viewBox="0 0 24 24">
-                <path d="M12 4 L21 21 H3 Z" style="fill:#ff9800; stroke:#ff9800; stroke-width:4; stroke-linejoin:round;" />
-                <circle cx="12" cy="18" r="1.3" fill="#fff" />
-                <rect x="11" y="7" width="2" height="8" fill="#fff" rx="2" />
-            </svg>`
-    };
-
+    getSvgIcons(color) {
+        const svgIcons = {
+            confirm: `
+                <svg width="90" height="68" style="margin: -18px 0px -5px -25px;" xmlns="http://www.w3.org/2000/svg">
+                    <rect width="40" height="40" x="25" y="25" fill="${color || '#ffc107'}" transform="rotate(45 60 60)"></rect>
+                    <text x="60" y="42" font-size="27" font-weight="bold" text-anchor="middle" dominant-baseline="middle" fill="white">?</text>
+                </svg>`,
+            success: `
+                <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 24 24">
+                    <circle cx="12" cy="12" r="10" fill="${color || '#07a545'}" stroke="${color || '#07a545'}" stroke-width="2"/>
+                    <path d="M8 12.5l3 3 5-6" fill="none" stroke="#fff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                </svg>`,
+            failed: `
+                <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="${color || '#d40404'}" viewBox="0 0 24 24">
+                    <path fill-rule="evenodd" d="M2 12C2 6.477 6.477 2 12 2s10 4.477 10 10-4.477 10-10 10S2 17.523 2 12Zm7.707-3.707a1 1 0 0 0-1.414 1.414L10.586 12l-2.293 2.293a1 1 0 1 0 1.414 1.414L12 13.414l2.293 2.293a1 1 0 0 0 1.414-1.414L13.414 12l2.293-2.293a1 1 0 0 0-1.414-1.414L12 10.586 9.707 8.293Z" clip-rule="evenodd"/>
+                </svg>`,
+            warning: `
+                <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="${color || '#ff9800'}" viewBox="0 0 24 24">
+                    <path d="M12 4 L21 21 H3 Z" style="fill:${color || '#ff9800'}; stroke:${color || '#ff9800'}; stroke-width:4; stroke-linejoin:round;" />
+                    <circle cx="12" cy="18" r="1.3" fill="#fff" />
+                    <rect x="11" y="7" width="2" height="8" fill="#fff" rx="2" />
+                </svg>`,
+            default: `
+                <svg fill="${color || '#17a2b8'}" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 416.979 416.979" width="45" height="45">
+                    <path d="M356.004,61.156c-81.37-81.47-213.377-81.551-294.848-0.182c-81.47,81.371-81.552,213.379-0.181,294.85
+                    c81.369,81.47,213.378,81.551,294.849,0.181C437.293,274.636,437.375,142.626,356.004,61.156z M237.6,340.786
+                    c0,3.217-2.607,5.822-5.822,5.822h-46.576c-3.215,0-5.822-2.605-5.822-5.822V167.885c0-3.217,2.607-5.822,5.822-5.822h46.576
+                    c3.215,0,5.822,2.604,5.822,5.822V340.786z M208.49,137.901c-18.618,0-33.766-15.146-33.766-33.765
+                    c0-18.617,15.147-33.766,33.766-33.766c18.619,0,33.766,15.148,33.766,33.766C242.256,122.755,227.107,137.901,208.49,137.901z"></path>
+                </svg>`
+        };
+    
+        return svgIcons;
+    }
+    
     // Create a reusable DOM structure for both types of pop-ups using template literals
     createPopupStructure() {
         // Define the styles in variables
@@ -123,8 +135,7 @@ class PopupHandler {
                 <div id="modal--content" style="${modalContentStyle}">
                     <div id="popup--header" style="padding: 2.3px; border-radius: 5px 5px 0 0"></div>
                     <div id="popup--content" style="${popupContentStyle}">
-                        <div id="popup--icon">
-                        </div>
+                        <div id="popup--icon"></div>
                         <div id="text--container" style="${textContainerStyle}">
                             <h3 id="popup--status" style="${popupStatusStyle}"></h3>
                             <span id="popup--msg" style="${popupMsgStyle}"></span>
@@ -169,31 +180,29 @@ class PopupHandler {
 
     /**
      * Show a popup with a custom message, status, and optional callback for confirmation actions.
-     * @param {Object} popupConfig - Configuration for displaying the popup.
+     * @param {Object} popupConfig - Configuration object for displaying the popup.
      * @param {string} popupConfig.message - The message to display inside the popup.
-     * @param {string} popupConfig.status - The type or status of the popup (e.g., "confirm", "success", "failed", "warning", "error", 'image').
+     * @param {string} popupConfig.status - The type or status of the popup (e.g., "confirm", "success", "failed", "fail", "error", "warning", "image", "default").
      * @param {function} [popupConfig.callback] - Optional callback function executed when the confirm button is clicked.
-     * @param {string} [popupConfig.actionButtonText] - Optional text for the action/confirm button.
-     * * @param {string} [popupConfig.imageUrl] - Optional URL for displaying an image in the popup (used for the "image" status).
+     * @param {string} [popupConfig.actionButtonText] - Optional custom text for the action/confirm button.
+     * @param {string} [popupConfig.imageUrl] - Optional URL for displaying an image in the popup (used for the "image" status).
+     * @param {string} [popupConfig.color] - Optional color to customize the appearance of the popup (e.g., header color, icon color and status color).
+     * @param {string} [popupConfig.svgIcon] - Optional svg icon to customize the appearance of the popup.
      */
+
     showPopup(popupConfig) {
-        const { message, status, callback, actionButtonText, imageUrl } = popupConfig;
+        const { message, status, callback, actionButtonText, imageUrl, color, svgIcon } = popupConfig;
 
         // Ensure status is lowercase for consistency
-        const lowerStatus = status.toLowerCase();
+        const lowerStatus = status !== undefined ? status.toLowerCase() : 'status' ;
 
         if (status === 'image' && !imageUrl) { 
             console.error("Image URL not provided to showPopup");
             return;
         }
         // Configurations based on status type
-        const statusConfig = this.getStatusConfig(lowerStatus, actionButtonText, callback, imageUrl);
+        const statusConfig = this.getStatusConfig(lowerStatus, actionButtonText, callback, imageUrl, color, svgIcon);
 
-        if (!statusConfig) {
-            console.error("Invalid status provided to showPopup");
-            return;
-        }
-        
         // Apply styles and content to popup elements
         this.applyPopupStylesAndContent(lowerStatus, message, statusConfig);
 
@@ -209,61 +218,69 @@ class PopupHandler {
      * @param {string} status - The status type of the popup.
      * @param {string} [actionButtonText] - Optional custom text for the action button.
      * @param {function} [callback] - Optional callback function for confirmation actions.
-     * @returns {Object|null} The status configuration object or null if the status is invalid.
      * @param {string} [imageUrl] - Optional URL for displaying an image in the popup (used for the "image" status).
+     * @param {string} [color] - Optional color for displaying in the popup.
+     * @param {string} [svgIcon] - Optional svg icon for displaying in the popup.
+     * @returns {Object|default} The status configuration object or default.
      */
-    getStatusConfig(status, actionButtonText, callback, imageUrl) {
+    getStatusConfig(status, actionButtonText, callback, imageUrl, color, svgIcon) {
+        const svgIcons = this.getSvgIcons(color)
+
+        const commonFailedConfig = {
+            iconSVG: svgIcon || svgIcons.failed,
+            color: color || '#d40404',
+            actionText: actionButtonText || 'Ok',
+            cancelButton: 'none',
+            actionHandler: () => this.handleActionWithCallback(callback)
+        };
+
         const statusOptions = {
             confirm: {
-                iconSVG: this.svgIcons.confirm,
-                color: '#ffc107',
+                iconSVG: svgIcon || svgIcons.confirm,
+                color: color || '#ffc107',
                 actionText: actionButtonText || 'Confirm',
                 cancelButton: 'inline-block',
                 actionHandler: () => this.handleActionWithCallback(callback)
             },
             success: {
-                iconSVG: this.svgIcons.success,
-                color: '#07a545',
+                iconSVG: svgIcon || svgIcons.success,
+                color: color || '#07a545',
                 actionText: actionButtonText || 'Done',
                 cancelButton: 'none',
                 actionHandler: () => this.handleActionWithCallback(callback)
             },
-            failed: {
-                iconSVG: this.svgIcons.failed,
-                color: '#d40404',
-                actionText: actionButtonText || 'Ok',
-                cancelButton: 'none',
-                actionHandler: () => this.handleActionWithCallback(callback)
-            },
-            error: {
-                iconSVG: this.svgIcons.failed,
-                color: '#d40404',
-                actionText: actionButtonText || 'Ok',
-                cancelButton: 'none',
-                actionHandler: () => this.handleActionWithCallback(callback)
-            },
+            failed: commonFailedConfig,
+            error: commonFailedConfig,
+            fail: commonFailedConfig,
             warning: {
-                iconSVG: this.svgIcons.warning,
-                color: '#ff9800',
+                iconSVG: svgIcon || svgIcons.warning,
+                color: color || '#ff9800',
                 actionText: actionButtonText || 'Ok',
                 cancelButton: 'none',
                 actionHandler: () => this.handleActionWithCallback(callback)
             },
             image: {
                 iconSVG: `<img src="${imageUrl}" style="width: 100%;" alt="popup image">`,
-                color: '#fff',
+                color: color || '#fff',
                 actionText: actionButtonText || 'Close',
                 cancelButton: 'none',
                 actionHandler: () => this.hidePopup()
+            },
+            default: {
+                iconSVG: svgIcon || svgIcons.default,
+                color: color || '#17a2b8',
+                actionText: actionButtonText || 'Ok',
+                cancelButton: 'inline-block',
+                actionHandler: () => this.handleActionWithCallback(callback)
             }
         };
 
-        return statusOptions[status] || null;
-    }
+        return statusOptions[status] || statusOptions.default;
+    } 
 
     /**
      * Apply the styles and content to the popup elements based on the provided configuration.
-     * @param {string} status - The status to display in the popup (e.g., "confirm", "success", "failed", "warning", "error", 'image').
+     * @param {string} status - The status to display in the popup (e.g., "confirm", "success", "failed", "fail", "error", "warning", 'image', 'default').
      * @param {string} message - The message to display in the popup.
      * @param {Object} config - The configuration object containing styles, icons, and button settings.
      */
@@ -271,7 +288,7 @@ class PopupHandler {
         this.modal.style.display = 'flex';
         this.header.style.background = config.color;
         this.popupStatus.textContent = status === 'confirm' ? 'Are You Sure?' : status === 'image' ? ''  : status.toUpperCase();
-        this.popupMsg.textContent = message;
+        this.popupMsg.textContent = message !== undefined ? message : 'Message';
         this.icon.innerHTML = config.iconSVG;
         this.popupStatus.style.color = config.color;
         this.cancelButton.style.display = config.cancelButton;
